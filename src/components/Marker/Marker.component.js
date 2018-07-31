11cd6168f5229a2f87b52f6ff4b1a35772b34471
Stu <stu@pilotinteractive.ca>
2018-07-30 22:36:09 -0400

    Fix styled component bug

--- src/components/Marker/Marker.component.js
+++ src/components/Marker/Marker.component.js
@@ -2,13 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../design-system';
 
+const Progress = Box.extend.attrs({
+  style: ({ progress }) => ({
+    width: `${progress * 100}%`,
+  }),
+})``;
+
+const Container = Box.extend`
+  overflow: hidden;
+`;
+
 export const MarkerComponent = ({ progress, children }) => (
-  <Box width={1} flex="1 1 auto" display="flex" position="relative">
-    <Box width={progress} height="100%" bg="white" opacity="0.2" position="absolute" />
+  <Container width={1} flex="1 1 auto" display="flex" position="relative">
+    <Progress progress={progress} height="100%" bg="white" opacity="0.2" position="absolute" />
     <Box position="absolute" display="flex" width="100%">
       {children}
     </Box>
-  </Box>
+  </Container>
 );
 
 MarkerComponent.propTypes = {
