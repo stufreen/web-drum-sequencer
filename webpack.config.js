54aa4c81b1076fd909787144977ea5b52282f5ce
Stu Freen <stuart.freen@rangle.io>
2018-08-09 18:17:55 -0400

    Add dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,6 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
               outputPath: './assets/fonts/',
             },
           },
