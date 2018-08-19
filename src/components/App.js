799741159e305f73de76b267293dfcbbb382d93d
Stu Freen <stuart.freen@rangle.io>
2018-08-19 13:09:49 -0400

    Remove tag line

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ import {
   ChannelHeader,
   MasterControls,
   Logo,
-  Text,
 } from '.';
 
 globalStyles();
@@ -19,9 +18,6 @@ const App = () => (
   <ThemeProvider theme={theme}>
     <Box className="App" p={[0, 0, 0, 1, 2, 2, 2, 3, 5]} pt={[4, 4, 4, 4, 4, 4, 4, 4, 5]}>
       <Logo color="white" width="12rem" />
-      <Text fontWeight="400" color="steel" ml={1} fontSize={1}>
-        Web Drum Sequencer
-      </Text>
       <Line bg="lightGray" mb={4} mt={4} />
       <MasterControls />
       <ChannelHeader />
