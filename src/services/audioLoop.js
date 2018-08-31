bb437154506023d12118915c85e935eb98ed365b
Stu Freen <stuart.freen@rangle.io>
2018-08-31 15:49:38 -0400

    Fix bug with audio loop; Rebuild

--- src/services/audioLoop.js
+++ src/services/audioLoop.js
@@ -17,20 +17,22 @@ export const initializeAudio = (store) => {
     updateChannelNodes(channels);
 
     if (playbackSession.playing) {
-      scheduleNotes({
-        notes,
-        channels,
-        startTime: playbackSession.startTime,
-        bpm: tempo.bpm,
-        pattern: master.pattern,
-        currentBeat: getCurrentBeat(tempo.bpm, playbackSession.startTime),
-      });
-
+      let sT = playbackSession.startTime;
       // Loop if we reached the end of the bar
       const barLength = 4 * 60 / tempo.bpm;
       if (audioCtx.currentTime > playbackSession.startTime + barLength) {
         store.dispatch(setStartTime(playbackSession.startTime + barLength));
+        sT = playbackSession.startTime + barLength;
       }
+
+      scheduleNotes({
+        notes,
+        channels,
+        startTime: sT,
+        bpm: tempo.bpm,
+        pattern: master.pattern,
+        currentBeat: getCurrentBeat(tempo.bpm, playbackSession.startTime),
+      });
     }
   }, INTERVAL);
 };
