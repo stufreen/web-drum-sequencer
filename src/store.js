412e69b7e1cf9968827ad0784d7efb9d8e89a2b2
Stu <stu@pilotinteractive.ca>
2018-09-03 11:34:17 -0400

    Rebuild; Update redux persist to use versions;

--- src/store.js
+++ src/store.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import reducer from './reducer';
 
+const migrations = {
+  1: () => ({ }),
+};
+
 export const configureStore = (callback) => {
   const persistConfig = {
-    key: 'root',
+    key: 'root-v0.1.0',
     storage,
-    version: 0,
+    version: 1,
     blacklist: ['playbackSession', 'window'],
+    migrate: createMigrate(migrations, { debug: process.env.__DEV__ }), // eslint-disable-line
   };
 
   const persistedReducer = persistReducer(persistConfig, reducer);
