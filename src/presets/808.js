2cb46c82fac4f835b70714a8554e0cd144c72633
Stu <stu@pilotinteractive.ca>
2018-09-05 07:46:00 -0400

    Add 808 preset

---
+++ src/presets/808.js
@@ -0,0 +1,865 @@
+export default {
+  name: '808',
+  bpm: 105,
+  channels: [
+    {
+      id: 'empty_channel',
+      sample: {
+        name: '808 Bass Short',
+        url: 'assets/audio/808-bd-short.mp3',
+      },
+      gain: 0.83,
+    },
+    {
+      id: 'eb405dca-32d1-4867-b7e0-ef37198f5fee',
+      sample: {
+        name: '808 Bass Long',
+        url: 'assets/audio/808-bd-long.mp3',
+      },
+      gain: 1,
+    },
+    {
+      id: '59fe4d57-c587-441b-b10d-9a2b54d588fd',
+      sample: {
+        name: '808 Snare',
+        url: 'assets/audio/808-sd.mp3',
+      },
+      gain: 1,
+    },
+    {
+      id: '6a2a5e54-8cbc-4e7e-a81c-68fc4b34c0bc',
+      sample: {
+        name: '808 Clap',
+        url: 'assets/audio/808-clap.mp3',
+      },
+      gain: 0.85,
+    },
+    {
+      id: '9b6de154-8a6e-4941-80db-4b9f47d9c241',
+      sample: {
+        name: '808 Hi-hat Closed',
+        url: 'assets/audio/808-ch.mp3',
+      },
+      gain: 0.21,
+    },
+    {
+      id: 'fb2edb2c-3d21-49fd-98d8-459b153fb083',
+      sample: {
+        name: '808 Hi-hat Open',
+        url: 'assets/audio/808-oh.mp3',
+      },
+      gain: 0.28,
+    },
+    {
+      id: '27f949f4-f5f7-4846-abfb-1d0e544b5bff',
+      sample: {
+        name: '808 Cowbell',
+        url: 'assets/audio/808-cowbell.mp3',
+      },
+      gain: 0.55,
+    },
+    {
+      id: 'd8d65425-e980-43a6-b3b3-1c14b55b8c94',
+      sample: {
+        name: '808 Cymbal',
+        url: 'assets/audio/808-cym.mp3',
+      },
+      gain: 0.37,
+    },
+    {
+      id: 'dbbfd24a-0f5d-409f-b4ed-ec330b47e2e5',
+      sample: {
+        name: '808 Clave',
+        url: 'assets/audio/808-clav.mp3',
+      },
+      gain: 0.31,
+    },
+    {
+      id: '206fcf08-67ff-4e98-905e-2495383fb83c',
+      sample: {
+        name: '808 Rimshot',
+        url: 'assets/audio/808-rs.mp3',
+      },
+      gain: 0.43,
+    },
+    {
+      id: '0ea2f943-2144-4434-92f7-8d689d49633a',
+      sample: {
+        name: '808 High Tom',
+        url: 'assets/audio/808-ht.mp3',
+      },
+      gain: 0.71,
+    },
+    {
+      id: '2f090e95-37f7-4c49-b0c5-eb35d64216c8',
+      sample: {
+        name: '808 Mid Tom',
+        url: 'assets/audio/808-mt.mp3',
+      },
+      gain: 0.59,
+    },
+    {
+      id: '816f7250-0afb-4353-bc6c-84ad97f9b8c6',
+      sample: {
+        name: '808 Low Tom',
+        url: 'assets/audio/808-lt.mp3',
+      },
+      gain: 0.71,
+    },
+  ],
+  notes: {
+    empty_channel: [
+      [],
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: '8d680b7a-2d25-4e13-867f-8b6b8b6415b0',
+        },
+        {
+          beat: 3,
+          id: '420a2676-67a4-4006-a068-22cdb19fb5ac',
+        },
+        {
+          beat: 4,
+          id: '91ffc7b1-ef92-452e-99df-02ce05d01277',
+        },
+        {
+          beat: 2,
+          id: 'a044d8e8-9a4a-4221-8f81-071897fb94a9',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 3.5,
+          id: 'b32de0b1-4eb6-484f-8cb8-b6550bf7d5ff',
+        },
+        {
+          beat: 4.25,
+          id: '5f017b71-3a67-4735-83b0-b4dc5eb0e55d',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: '5c7e76ca-fdf3-4f39-8379-d847298dcbe8',
+        },
+        {
+          beat: 2,
+          id: '9630e240-98a3-482b-a956-1b1cbbc3f6f7',
+        },
+        {
+          beat: 3,
+          id: '64f339ef-5a10-4b23-8dfb-ad68c6b8a3dc',
+        },
+        {
+          beat: 4,
+          id: '0471ef77-08b4-4d5a-815b-3a7b2541513a',
+        },
+      ],
+      [],
+    ],
+    'eb405dca-32d1-4867-b7e0-ef37198f5fee': [
+      [
+        {
+          beat: 1,
+          id: '29ddd774-51b9-47eb-aa90-15b5647af688',
+        },
+        {
+          beat: 2.5,
+          id: '97fc68b1-722d-4f86-bd01-bb7dca96d6e5',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: 'fd0bfd6d-8faa-4e78-9d7c-d3c176a0c2fc',
+        },
+        {
+          beat: 3.5,
+          id: 'fe3b3f20-fa95-4204-86ae-eb30e63f8b49',
+        },
+      ],
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: '032ecd95-d72e-426d-a742-a5deb4bcfb88',
+        },
+        {
+          beat: 3.5,
+          id: '29082399-8b29-4ce7-829c-6ae8d15da51c',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: '86b85f42-dcdb-44a8-a0eb-425d4bf97ffa',
+        },
+        {
+          beat: 2.5,
+          id: '15f8b82c-df03-41e8-ba72-4087c1c90663',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 1,
+          id: '6fc5428e-80b6-4d11-a3bd-42aefb1230f6',
+        },
+      ],
+    ],
+    '59fe4d57-c587-441b-b10d-9a2b54d588fd': [
+      [
+        {
+          beat: 4,
+          id: '4e31c684-0f4c-47f1-a891-50709c3bf1fc',
+        },
+        {
+          beat: 2,
+          id: '64743cf2-27a8-4ad6-af11-621f5b2acdfa',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '4600798b-3b4a-4465-a494-4a071d9d4d84',
+        },
+        {
+          beat: 4,
+          id: '32205ee0-e933-493f-af20-eece6f97e2ab',
+        },
+      ],
+      [
+        {
+          beat: 4.75,
+          id: '25531c10-3a3f-425a-8814-7800830a2cde',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '8c3a23f5-e6d8-458c-92de-c1f92be0f0f9',
+        },
+        {
+          beat: 4,
+          id: 'b3af60f9-8a57-4a00-ac9b-0175eafffc77',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '88e1fa22-f678-4cd2-926b-296cd1a09948',
+        },
+        {
+          beat: 4,
+          id: '5dab556e-f303-4ed2-b85c-9ec47dbf026d',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: 'c0ed62e3-2763-4bf5-b80c-58aa5154cf99',
+        },
+        {
+          beat: 4,
+          id: '95355109-8f8e-4964-b44d-831a6c3f1433',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 3.5,
+          id: '3cf162e3-5d2d-4ff9-b2b6-6aa372280ea8',
+        },
+        {
+          beat: 4.5,
+          id: '401eeb6f-ec89-4b57-b687-9d87b0f7d04f',
+        },
+        {
+          beat: 4.75,
+          id: '9ddab637-94c1-438e-9ec7-331df982d034',
+        },
+        {
+          beat: 4,
+          id: '0c0b94fe-ceda-424b-8d8b-7c717f13ec01',
+        },
+      ],
+    ],
+    '6a2a5e54-8cbc-4e7e-a81c-68fc4b34c0bc': [
+      [
+        {
+          beat: 2,
+          id: '0b7163d9-dfdc-4e0c-b5b0-f7730c198f54',
+        },
+        {
+          beat: 4,
+          id: '0ec12d1a-8ed9-4f44-99d4-d2a9cbc8cd99',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '4a11b3b3-81c2-4bd8-8b27-de87dabe02ac',
+        },
+        {
+          beat: 2.75,
+          id: '71160d69-5717-4369-99bf-e15dcf4c7bc6',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '0fc99548-9184-4ff0-8531-cbec8a5c11c2',
+        },
+        {
+          beat: 4,
+          id: '78b6b419-5274-4b35-a1aa-d05bed345509',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 2.75,
+          id: '8b8d953f-3345-40af-90ce-3d1f786f7bdb',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '2e01cf3a-15c4-4693-990d-c237c5691c82',
+        },
+        {
+          beat: 4,
+          id: 'af6bc3b9-479c-4efe-b06a-355a44991302',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: 'd389f8c8-565c-4d31-a9bb-ca3f72cb8a07',
+        },
+        {
+          beat: 4,
+          id: 'ab33470e-2ee2-421b-9541-e0fe340f32a6',
+        },
+      ],
+      [
+        {
+          beat: 2,
+          id: '9ad61605-2b9c-407b-aa82-45972b2bd4aa',
+        },
+        {
+          beat: 4,
+          id: '5c475e3c-8487-4d0d-8ec0-d3afacaaa556',
+        },
+      ],
+    ],
+    '9b6de154-8a6e-4941-80db-4b9f47d9c241': [
+      [
+        {
+          beat: 1,
+          id: 'ea650f53-6330-492d-a92e-47ad46e49c8b',
+        },
+        {
+          beat: 1.5,
+          id: '76e109d2-7a93-475a-a1ca-e635262e824b',
+        },
+        {
+          beat: 2,
+          id: 'be5d8b74-1d00-4ce7-a075-1f4770bdeb04',
+        },
+        {
+          beat: 2.5,
+          id: '09aa3eba-d22b-4987-b1bd-07d796b055e1',
+        },
+        {
+          beat: 3,
+          id: '9d4f42eb-ad56-45ea-a183-6034047786fa',
+        },
+        {
+          beat: 4,
+          id: 'd151c78e-dee5-4e3a-bb08-d8916c226d70',
+        },
+        {
+          beat: 3.5,
+          id: '2dce2fae-5413-40ef-9192-49d00dee623c',
+        },
+      ],
+      [
+        {
+          beat: 1.5,
+          id: '2942f843-440c-46e9-aa5e-8cebee9e86f3',
+        },
+        {
+          beat: 2.5,
+          id: '1598985e-fbda-4325-9df7-4e4fdd426061',
+        },
+        {
+          beat: 3.5,
+          id: '01140c00-7e38-4c47-bb2f-866c87e9c2bf',
+        },
+        {
+          beat: 4.5,
+          id: 'c8eb2e22-36b7-4891-96a4-75baaefe1cd8',
+        },
+      ],
+      [
+        {
+          beat: 1.5,
+          id: '7d6d2ef5-50e6-4d25-b583-416ea1b67a73',
+        },
+        {
+          beat: 1,
+          id: '955cb6a2-05a1-4c72-9436-2c9da602f3a0',
+        },
+        {
+          beat: 2,
+          id: 'e9cbcf6f-acde-4cb4-a5c7-334fcf2f9854',
+        },
+        {
+          beat: 2.5,
+          id: '472ac95d-e5a3-4938-84cc-e6b8204833bf',
+        },
+        {
+          beat: 2.75,
+          id: '5f69d201-c2d9-4051-83bf-34fd4c5346e4',
+        },
+        {
+          beat: 3.5,
+          id: 'bdf0b3dc-cfe4-4384-84e1-80a390f6e165',
+        },
+        {
+          beat: 4,
+          id: '3a1d6250-7a3e-4bea-93fe-cc1f0015534c',
+        },
+        {
+          beat: 4.5,
+          id: 'ddb60cd9-b9e3-4bf5-810c-2824ef1241ba',
+        },
+      ],
+      [
+        {
+          beat: 1.5,
+          id: '2c2fb20d-c409-4fee-bc44-e35e9b494cc3',
+        },
+        {
+          beat: 2.5,
+          id: '613f728c-16ff-4d4b-9f74-84d2923ad13d',
+        },
+        {
+          beat: 3.5,
+          id: 'f1a762f3-4481-44d3-9b31-ae3d488c54c6',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: '6edb0a4b-c91f-42dc-bb43-2e54eee77288',
+        },
+        {
+          beat: 1.5,
+          id: 'ae2f66d3-100d-431c-a3fb-c7c8ddd1c2f3',
+        },
+        {
+          beat: 2,
+          id: 'f0543739-d954-435c-a5fe-93d72ee582f2',
+        },
+        {
+          beat: 3,
+          id: 'a2edccea-0cf4-4947-b149-4191f07bdf1e',
+        },
+        {
+          beat: 3.5,
+          id: 'cee93951-856d-4093-9cea-2f3b516f792d',
+        },
+        {
+          beat: 4,
+          id: '4c604170-949c-4ff9-8789-01bd72d55f46',
+        },
+        {
+          beat: 4.5,
+          id: 'a4f3ad08-12bd-4e5e-9a2a-1f0e0b53b4d5',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: 'c4ef6a9a-bd43-4878-8753-3a4ef5813266',
+        },
+        {
+          beat: 1.25,
+          id: 'af3c946e-b413-4dfd-9a8e-cc9277edd88c',
+        },
+        {
+          beat: 1.5,
+          id: '3c60b720-f348-445e-a9e3-74a5ac94c9bf',
+        },
+        {
+          beat: 1.75,
+          id: '36f75f4b-12ad-4f10-a563-f243cdac6492',
+        },
+        {
+          beat: 2,
+          id: '14582379-11b5-4496-ac28-ef2fc9ff92c8',
+        },
+        {
+          beat: 2.25,
+          id: '2cbb4cf7-d2c5-4b73-bd09-12476967bc32',
+        },
+        {
+          beat: 2.5,
+          id: 'b4a6edab-10c5-4b48-927f-fe36d8711e4b',
+        },
+        {
+          beat: 2.75,
+          id: 'b48dcdb6-085b-43f6-a5f5-430d5f357425',
+        },
+        {
+          beat: 3,
+          id: 'e69253b1-bedc-4126-843c-e8373732b347',
+        },
+        {
+          beat: 3.25,
+          id: 'c6d5f82a-699b-4b78-a37d-053d4d3dcb5b',
+        },
+        {
+          beat: 3.75,
+          id: 'b171d982-949f-41c7-96e5-34461596e5ab',
+        },
+        {
+          beat: 4,
+          id: '193c8efc-41e8-415f-9eb9-e3130cd5705f',
+        },
+        {
+          beat: 4.25,
+          id: '9300786a-def9-4a5c-afbb-576342fb80f5',
+        },
+        {
+          beat: 4.75,
+          id: 'cf3053bf-0e9c-4d2c-ac08-b47ee626b288',
+        },
+      ],
+      [
+        {
+          beat: 1.5,
+          id: 'ef337ebe-946a-47c1-aedd-450564a97014',
+        },
+        {
+          beat: 2.5,
+          id: '9bca0ede-1f8e-4e0d-8af8-c5999609c001',
+        },
+        {
+          beat: 3.5,
+          id: '3a5729e6-34f5-47e6-8d50-7519415ca13b',
+        },
+        {
+          beat: 4.5,
+          id: 'ab176a41-9776-497a-af60-07ec36b23606',
+        },
+      ],
+      [
+        {
+          beat: 1.5,
+          id: 'e2dbd7b2-1f0e-4dc8-b815-10beb669ff27',
+        },
+        {
+          beat: 2.5,
+          id: 'ddeb3419-3116-43d7-a0f2-34e46e4ae886',
+        },
+        {
+          beat: 3.5,
+          id: '9820216b-00d6-4295-b0b9-ea809e81e782',
+        },
+        {
+          beat: 4.5,
+          id: 'f1b516d1-a2fb-43d2-b42e-1596d3191b0d',
+        },
+      ],
+    ],
+    'fb2edb2c-3d21-49fd-98d8-459b153fb083': [
+      [
+        {
+          beat: 4.5,
+          id: '77238ce7-2fc0-44c2-8af2-789edaeba92e',
+        },
+      ],
+      [],
+      [],
+      [
+        {
+          beat: 4.5,
+          id: '03b7e1b0-2fb8-4dca-b982-6f0c77ae6673',
+        },
+      ],
+      [
+        {
+          beat: 2.5,
+          id: '07eb2589-17e6-45d8-b18b-5b4d4a0aa8f5',
+        },
+      ],
+      [
+        {
+          beat: 3.5,
+          id: 'afba7c4b-7f29-4602-8852-602d138f6946',
+        },
+        {
+          beat: 4.5,
+          id: '26924a16-d4c6-4b7f-b257-aaeb155db511',
+        },
+      ],
+      [],
+      [],
+    ],
+    '27f949f4-f5f7-4846-abfb-1d0e544b5bff': [
+      [
+        {
+          beat: 2.75,
+          id: '0f04bb15-6c36-4c58-974d-6aa004382576',
+        },
+        {
+          beat: 3.5,
+          id: 'e1e4bb50-c57b-4260-89a2-b3726635cfd4',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 1.5,
+          id: '24746c40-9fb0-483c-939c-3842b767b1d4',
+        },
+        {
+          beat: 4.75,
+          id: 'e7d0ff1f-fa09-431e-9a1a-fc6aa7cf0a3f',
+        },
+      ],
+      [],
+      [],
+      [
+        {
+          beat: 1.5,
+          id: '3d7b1deb-47dc-4029-b89e-0d701691b963',
+        },
+        {
+          beat: 4.75,
+          id: 'afb4d24b-41e1-4388-a724-04d90a637991',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 1,
+          id: '08e0071c-c3a3-4a96-aa1d-8c770c3b4c27',
+        },
+        {
+          beat: 1.5,
+          id: 'dee01a92-967a-4af0-8a19-a2899b5044e0',
+        },
+      ],
+    ],
+    'd8d65425-e980-43a6-b3b3-1c14b55b8c94': [
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: '29d70f6a-df60-47d3-bbb5-5031c29e4390',
+        },
+      ],
+      [],
+      [],
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: '03a3e0e5-9278-4d51-a41c-6ec04de5de0f',
+        },
+      ],
+    ],
+    'dbbfd24a-0f5d-409f-b4ed-ec330b47e2e5': [
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: 'b1ce6e18-9e22-405f-bb53-0b60a2c0c2c3',
+        },
+        {
+          beat: 3,
+          id: '64e011d1-93e0-46ac-b6e1-c51955edece1',
+        },
+      ],
+      [
+        {
+          beat: 1,
+          id: '34dc07d7-1698-41be-bb7b-973f38a6dc16',
+        },
+        {
+          beat: 1.25,
+          id: 'a5e62b92-f494-431d-a325-1b747b27186b',
+        },
+        {
+          beat: 2,
+          id: '62e8cae9-f00d-47db-b985-b6acf219ed54',
+        },
+        {
+          beat: 2.75,
+          id: '7cea74a4-dd0d-43bf-9db3-f5b44c0de7f6',
+        },
+        {
+          beat: 2.5,
+          id: '5dd16303-e248-4b87-b988-ade5d68a0160',
+        },
+        {
+          beat: 3.5,
+          id: '20e36003-3b5d-43cb-a966-53d94bfe3379',
+        },
+        {
+          beat: 4.5,
+          id: '81d5797e-4c59-4a1d-ad4b-a77b916c757d',
+        },
+        {
+          beat: 4,
+          id: '53644024-31c7-4c18-a116-e2bca5e7fa36',
+        },
+      ],
+      [],
+      [],
+      [],
+      [],
+    ],
+    '206fcf08-67ff-4e98-905e-2495383fb83c': [
+      [],
+      [
+        {
+          beat: 3.5,
+          id: '34424649-30bb-4195-bc4c-46e5cb2dbd5c',
+        },
+        {
+          beat: 3.75,
+          id: '957478f5-b2b5-4feb-8626-0b41121764f3',
+        },
+        {
+          beat: 4.25,
+          id: 'b5e9576d-7cbf-4c8d-86bf-06e0eb66d685',
+        },
+        {
+          beat: 4.5,
+          id: '9946c9a4-c59e-4b32-8574-f65536dc56a6',
+        },
+        {
+          beat: 4,
+          id: 'd9b5d411-bb73-46f4-91f2-befc027c3b99',
+        },
+      ],
+      [],
+      [],
+      [
+        {
+          beat: 1,
+          id: 'b9206d8e-272e-4e76-beee-ba008688cc67',
+        },
+        {
+          beat: 2.75,
+          id: 'd0e18285-2b00-4893-b8be-abad2a3f848b',
+        },
+        {
+          beat: 3.5,
+          id: '1f20cfc1-c299-416c-b141-4b42c05e16e9',
+        },
+        {
+          beat: 4.5,
+          id: 'f126fead-b0b9-4aca-9f88-2ca16d46ab12',
+        },
+        {
+          beat: 1.5,
+          id: '523e145e-ea70-47c6-b977-4eced0d34b12',
+        },
+        {
+          beat: 2.25,
+          id: '43d1e8de-2c07-447f-9e9e-d4df8ec42c68',
+        },
+      ],
+      [],
+      [],
+      [],
+    ],
+    '0ea2f943-2144-4434-92f7-8d689d49633a': [
+      [],
+      [],
+      [],
+      [
+        {
+          beat: 1.75,
+          id: '69330ba3-a1d0-48a3-ba1e-f797c0571231',
+        },
+      ],
+      [
+        {
+          beat: 4.5,
+          id: 'e280cdfd-291a-4562-90a7-287613e891b5',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 1.75,
+          id: '10e9f6c6-698c-4d01-a9ea-508a213c435a',
+        },
+        {
+          beat: 3.75,
+          id: '97db3af1-342f-438c-a4ed-a45130b155ea',
+        },
+      ],
+      [],
+    ],
+    '2f090e95-37f7-4c49-b0c5-eb35d64216c8': [
+      [],
+      [],
+      [],
+      [
+        {
+          beat: 2.5,
+          id: '37346254-891c-45a9-8a55-8cf653e79e97',
+        },
+      ],
+      [],
+      [],
+      [],
+      [],
+    ],
+    '816f7250-0afb-4353-bc6c-84ad97f9b8c6': [
+      [],
+      [
+        {
+          beat: 2.5,
+          id: '1af0deed-c640-4d2c-8b31-8675eb13b914',
+        },
+      ],
+      [],
+      [
+        {
+          beat: 1,
+          id: '69ec2523-4eba-4ba0-83e2-35edb3ebc1a7',
+        },
+        {
+          beat: 3,
+          id: '3069e262-7b65-4da6-a401-eea332498f43',
+        },
+      ],
+      [],
+      [],
+      [
+        {
+          beat: 2.5,
+          id: '65bd6d4d-4b5e-45cd-ac0f-3ce821888d51',
+        },
+      ],
+      [],
+    ],
+  },
+};
--- src/presets/index.js
+++ src/presets/index.js
@@ -2,6 +2,7 @@ import empty from './empty';
 import hipHop from './hip-hop';
 import lDrum from './ldrum';
 import sevenohseven from './707';
+import eightoheight from './808';
 import ace from './ace';
 
-export default [empty, sevenohseven, ace, lDrum, hipHop];
+export default [empty, eightoheight, ace, lDrum, hipHop, sevenohseven];
