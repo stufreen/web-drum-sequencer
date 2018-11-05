88a0c7be1ec0c0049a1a550ae1cd80530bb5f47b
Stu Freen <stuart.freen@rangle.io>
2018-11-05 09:22:21 -0500

    Styling mute and solo buttons

--- src/components/MuteSolo/MuteSolo.component.js
+++ src/components/MuteSolo/MuteSolo.component.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Box, Button } from '../design-system';
 
 export const MuteSoloComponent = ({ onPressMuted, onPressSolo, channel}) => (
-  <Box>
-    <Button w="1rem" h="2rem" bg={channel.solo ? 'black' : 'yellow'} onClick={onPressSolo} />
-    <Button w="1rem" h="2rem" bg={channel.muted ? 'black' : 'red'} onClick={onPressMuted} />
+  <Box width="1.2rem" display="flex" ml={2} height="100%" flexDirection="column">
+    <Button width="1.2rem" flex="1 1 auto" mb={1} p={0} bg={channel.solo ? 'gold' : 'black'} onClick={onPressSolo} />
+    <Button width="1.2rem" flex="1 1 auto" p={0} bg={channel.muted ? 'brightRed' : 'black'} onClick={onPressMuted} />
   </Box>
 );
