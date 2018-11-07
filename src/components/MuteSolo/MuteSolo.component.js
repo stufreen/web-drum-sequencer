18f0717a2154729ca06f86c55eb9d06a1fd8188d
Stu Freen <stuart.freen@rangle.io>
2018-11-07 09:12:24 -0500

    Improve mute solo styling

--- src/components/MuteSolo/MuteSolo.component.js
+++ src/components/MuteSolo/MuteSolo.component.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { Box, Button } from '../design-system';
+import theme from '../../styles/theme';
+
+const MSButton = Button.extend`
+  width: 0.8rem;
+  height: 0.8rem;
+  padding: 0;
+  border-radius: 100%;
+  transition: all 0.2s;
+
+  &:hover {
+    box-shadow: 0 0 0 1.5px ${theme.colors.gray};
+  }
+`;
 
 export const MuteSoloComponent = ({ onPressMuted, onPressSolo, channel}) => (
   <Box
@@ -10,30 +23,13 @@ export const MuteSoloComponent = ({ onPressMuted, onPressSolo, channel}) => (
     height="100%"
     flexDirection="column"
   >
-    <Button
-      width="0.8rem"
-      height="0.8rem"
-      mb={1}
-      p={0}
-      bg="yellow"
-      borderRadius="50%"
+    <MSButton
+      bg={channel.solo ? 'yellow' : 'yellow30'}
       onClick={onPressSolo}
-      color="white"
-      fontSize={0}
-      fontWeight="300"
-      opacity={channel.solo ? 1 : 0.25}
     />
-    <Button
-      width="0.8rem"
-      height="0.8rem"
-      p={0}
-      bg="brightRed"
-      borderRadius="50%"
+    <MSButton
+      bg={channel.muted ? 'brightRed' : 'brightRed30'}
       onClick={onPressMuted}
-      color="white"
-      fontSize={0}
-      fontWeight="300"
-      opacity={channel.muted ? 1 : 0.25}
     />
   </Box>
 );
