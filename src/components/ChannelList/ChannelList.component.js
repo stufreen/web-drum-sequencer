6e7ffb3ab98c596dea4a596b7619f984957c5180
Stu Freen <stuart.freen@rangle.io>
2018-11-09 13:52:51 -0500

    Install draggable and get dragging working

--- src/components/ChannelList/ChannelList.component.js
+++ src/components/ChannelList/ChannelList.component.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Sortable } from '@shopify/draggable';
 import { Box } from '../design-system';
 import { Channel } from '../Channel';
 import { AddChannelButton } from '../AddChannelButton';
 
-export const ChannelListComponent = ({ channels }) => (
-  <Box mt={2}>
-    {channels.map(channel => <Channel key={channel.id} channel={channel} />)}
-    <AddChannelButton />
-  </Box>
-);
+export class ChannelListComponent extends React.Component {
+  componentDidMount() {
+    new Sortable([this.channelContainer], {
+      draggable: '.wds-draggable',
+      handle: '.wds-channel-handle',
+    }); 
+  }
+
+  render() {
+    const { channels } = this.props;
+    return (
+      <Box mt={2} innerRef={(el) => { this.channelContainer = el; }}>
+        {channels.map(channel => <Channel key={channel.id} channel={channel} />)}
+        <AddChannelButton />
+      </Box>
+    );
+  }
+}
 
 ChannelListComponent.propTypes = {
   channels: PropTypes.arrayOf(PropTypes.object).isRequired,
