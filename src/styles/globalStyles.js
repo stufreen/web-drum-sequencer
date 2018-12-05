b2cb82a748cb04941ddbae1bc4e95a94d02f6d41
Stu Freen <stuart.freen@rangle.io>
2018-12-05 18:00:08 -0500

    Clean up stylesheet with new font

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,51 +1,39 @@
 import { injectGlobal } from 'styled-components';
 import theme from './theme';
-// import josefinRegularWoff2 from '../assets/fonts/dtjakob-dflt-regular-webfont.woff2';
-// import josefinRegularWoff from '../assets/fonts/dtjakob-dflt-regular-webfont.woff';
-// import josefinBoldWoff2 from '../assets/fonts/dtjakobdflt-bold-webfont.woff2';
-// import josefinBoldWoff from '../assets/fonts/dtjakobdflt-bold-webfont.woff';
-// import josefinSemiboldWoff2 from '../assets/fonts/dtjakob-dflt-semibold-webfont.woff2';
-// import josefinSemiboldWoff from '../assets/fonts/dtjakob-dflt-semibold-webfont.woff';
-// import josefinRegularWoff2 from '../assets/fonts/wavehaus-book-webfont.woff2';
-// import josefinRegularWoff from '../assets/fonts/wavehaus-book-webfont.woff';
-// import josefinBoldWoff2 from '../assets/fonts/wavehaus-bold-webfont.woff2';
-// import josefinBoldWoff from '../assets/fonts/wavehaus-bold-webfont.woff';
-// import josefinSemiboldWoff2 from '../assets/fonts/wavehaus-semibold-webfont.woff2';
-// import josefinSemiboldWoff from '../assets/fonts/wavehaus-semibold-webfont.woff';
-import josefinRegularWoff2 from '../assets/fonts/jost-medium-webfont.woff2';
-import josefinRegularWoff from '../assets/fonts/jost-medium-webfont.woff';
-import josefinBoldWoff2 from '../assets/fonts/jost-bold-webfont.woff2';
-import josefinBoldWoff from '../assets/fonts/jost-bold-webfont.woff';
-import josefinSemiboldWoff2 from '../assets/fonts/jost-semi-webfont.woff2';
-import josefinSemiboldWoff from '../assets/fonts/jost-semi-webfont.woff';
+import jostMediumWoff2 from '../assets/fonts/jost-medium-webfont.woff2';
+import jostMediumWoff from '../assets/fonts/jost-medium-webfont.woff';
+import jostBoldWoff2 from '../assets/fonts/jost-bold-webfont.woff2';
+import jostBoldWoff from '../assets/fonts/jost-bold-webfont.woff';
+import jostSemiboldWoff2 from '../assets/fonts/jost-semi-webfont.woff2';
+import jostSemiboldWoff from '../assets/fonts/jost-semi-webfont.woff';
 
 export default () => injectGlobal`
-  /* josefin-sans-regular - latin */
+
   @font-face {
-    font-family: 'Josefin Sans';
+    font-family: 'Jost';
     font-style: normal;
     font-weight: 400;
-    src: local('Josefin Sans Regular'), local('JosefinSans-Regular'),
-        url(${josefinRegularWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-        url(${josefinRegularWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    src: local('Jost Medium'), local('Jost-Medium'),
+        url(${jostMediumWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url(${jostMediumWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
-  /* josefin-sans-600 - latin */
+
   @font-face {
-    font-family: 'Josefin Sans';
+    font-family: 'Jost';
     font-style: normal;
     font-weight: 600;
-    src: local('Josefin Sans SemiBold'), local('JosefinSans-SemiBold'),
-        url(${josefinSemiboldWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-        url(${josefinSemiboldWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    src: local('Jost SemiBold'), local('Jost-SemiBold'),
+        url(${jostSemiboldWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url(${jostSemiboldWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
-  /* josefin-sans-700 - latin */
+
   @font-face {
-    font-family: 'Josefin Sans';
+    font-family: 'Josts';
     font-style: normal;
     font-weight: 700;
-    src: local('Josefin Sans Bold'), local('JosefinSans-Bold'),
-        url(${josefinBoldWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-        url(${josefinBoldWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    src: local('Jost Bold'), local('Jost-Bold'),
+        url(${jostBoldWoff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url(${jostBoldWoff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
 
   html {
@@ -53,7 +41,7 @@ export default () => injectGlobal`
   }
 
   * {
-    font-family: "Josefin Sans", "Futura", sans-serif;
+    font-family: "Jost", "Futura", sans-serif;
     font-display: swap;
   }
 
